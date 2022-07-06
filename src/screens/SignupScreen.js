4f28ad6fbdbc579072e2dcf2be09ef589e52b0ac
Cem Safa <cemsafa@gmail.com>
2022-07-06 13:00:29 -0400

    SignIn & SignUp Screens

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,17 +1,42 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useContext } from "react";
+import { View, StyleSheet } from "react-native";
+import { NavigationEvents } from "react-navigation";
+import { Context as AuthContext } from "../context/AuthContext";
+import AuthForm from "../components/AuthForm";
+import NavLink from "../components/NavLink";
 
 const SignupScreen = () => {
+  const { state, signup, clearErrorMessage } = useContext(AuthContext);
+
   return (
-    <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>SignupScreen</Text>
+    <View style={styles.container}>
+      <NavigationEvents onWillFocus={clearErrorMessage} />
+      <AuthForm
+        headerText="Sign up"
+        buttonText="Sign Up"
+        errorMessage={state.errorMessage}
+        onSubmit={signup}
+      />
+      <NavLink
+        text="Already have and account? Sign in instead"
+        routeName="Signin"
+      />
     </View>
   );
 };
 
+SignupScreen.navigationOptions = () => {
+  return {
+    headerShown: false,
+  };
+};
+
 const styles = StyleSheet.create({
-  viewStyle: {},
-  textStyle: {},
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    marginBottom: 200,
+  },
 });
 
 export default SignupScreen;
